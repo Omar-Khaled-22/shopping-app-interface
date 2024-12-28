18600303e9b3f7bf2511e3bde7ae5e94fda7e059
OMAR\omark <omarkhaled2219@gmail.com>
2024-12-28 23:39:17 +0200

    -added a simple shopping app interface

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# shopping-app-interface
- a simplified version of a shopping app interface using Flutter.  This app will feature a main screen with multiple sections, displaying products, hot offers and recently purchased  products 
+# new_project
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/homePage.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: SingleChildScrollView(
+        //to make the page scrollable
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("Our Products"),
+            Center(
+              // container that has page view which contains an image
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: PageView(
+                  children: [
+                    Image.network(
+                        "https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc")
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Recently Purchased"),
+            //recently purchased section (Gridview with tw items in a row)
+            Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // 2 items per row
+                    crossAxisSpacing: 10.0, // Space between items horizontally
+                    mainAxisSpacing: 10.0, // Space between items vertically
+                  ),
+                  itemCount: 4, // Total number of items
+                  itemBuilder: (context, index) {
+                    return Column(
+                      //contain an image under it arow for the title and an icon button
+                      children: [
+                        Image.network(
+                            "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I"),
+                        Expanded(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text("data"),
+                              Center(
+                                child: IconButton(
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text("item added to the cart"),
+                                      ),
+                                    );
+                                  },
+                                  icon: Icon(Icons.add_box),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    );
+                  }),
+            ),
+
+            Text("Hot Offers"),
+            // hot offers section which contains a horizontal listview
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: ListView.builder(
+                scrollDirection:
+                    Axis.horizontal, // Set the scroll direction to horizontal
+                itemBuilder: (context, index) => Container(
+                  width: 200, // Set a fixed width for each item
+                  margin: const EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Image.network(
+                          "https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc"),
+                      const Text("data"),
+                    ],
+                  ),
+                ),
+                itemCount: 5,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
